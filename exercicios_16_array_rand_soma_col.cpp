6f91ea8d53373f431c8aea521d21661fce98f10a
Kai Wang <kaiuchihaa@gmail.com>
2022-01-15 00:09:10 -0300

    Novos Exercícios
    Realizei alguns exercícios novos com planilhas somando conlunas e linhas;

--- exercicios_16_array_rand_soma_col.cpp
+++ exercicios_16_array_rand_soma_col.cpp
@@ -5,41 +5,32 @@
 
 using namespace std;
 
-int main (){
-
+int main() {
 
     cout << endl << "===============================================================" << endl;
-    cout << "============EXEMPLOS DE ARRAY BIDIMENSIONAL COM FORM===============" << endl;
+    cout << "============EXEMPLO DE TEXTO PARA USAR NOS EX===============" << endl;
     cout << "===============================================================" << endl << endl;
 
-    double warray[5][6] = {};
-    int total = 0;
+    double warray[5][5]; // desta forma será gerado valores vázios para cadas célula da planilha.
 
     for (int x = 0; x < 5; x++){
-        int contatot = 0;
-        for (int y = 0; y < 6; y++){
-            if (y < 5){
-                warray[x][y] = (double)rand();
-                contatot += warray[x][y];
-            }
-            else{
-                warray[x][5] = contatot;
-            }
+        for (int y = 0; y < 5; x++){
 
-            string totit = (y < 4) ? " + " :
-                           (y != 5)? " = ":"\n";
-            cout << setw(6) << warray[x][y] << totit;
+            string totit = (y < 4) ? "|" : "\n";
+            warray[x][y] = (double) rand();
+            cout << setw(6) <<warray[x][y] << totit;
         }
-        total += contatot;
-    }
 
-    cout << "O TOTAL DA SOMA EH: " << total <<endl;
+    }
 
     cout << endl << "===============================================================" << endl;
     cout << "========================FIM DO PROGRAMA========================" << endl;
     cout << "===============================================================" << endl << endl;
 
-
     system("pause");
     return 0;
+
+
 }
+
+
